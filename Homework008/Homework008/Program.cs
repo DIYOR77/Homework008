fc25598910e56e1885ded7501eb332674a034cdc
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-06 11:51:15 +0500

    identify the sign 1

--- Homework008/Homework008/Program.cs
+++ Homework008/Homework008/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter number: ");
+            string son=Console.ReadLine();
+            if(double.TryParse(son, out double raqam)) 
+            {
+                Console.WriteLine(Find(raqam));
+            }
+            else
+            {
+                Console.WriteLine(" Faqat son kirita olasiz !!!");
+            }
+            Main(args);
+        }
+        public static string Find( double raqam)
+        {
+            if(raqam > 0)
+            {
+                return " 0 < entered number " +
+                    "+";
+            }
+            else if(raqam < 0)
+            {
+                return " 0 > entered number" +
+                    "-";
+            }
+            else
+            {
+                return " 0 = entered number" +
+                    "0";
+            }
         }
     }
 }
