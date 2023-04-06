755cb52b2efd16b1fca5b3ab92fe6e41817da3b8
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-06 12:14:22 +0500

    Honasini aniqlash

--- Homework008/Homework008/Program.cs
+++ Homework008/Homework008/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter number: ");
+            string son=Console.ReadLine();
+            if(int.TryParse(son, out int value))
+            {
+                Console.WriteLine(" Xona: "+Xonasinianiq(son));
+            }
+            else
+            {
+                Console.WriteLine(" Not an integer number !!!");
+            }
+            Main(args);
+        }
+        public static int Xonasinianiq(string son,int nuber)
+        { 
+                return son.Length;     
         }
     }
 }
