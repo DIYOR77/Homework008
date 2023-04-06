496df534734810a59d2197908ab2bad6c7160570
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-04-06 12:03:41 +0500

    Asrni aniqlash

--- Homework008/Homework008/Program.cs
+++ Homework008/Homework008/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter year: ");
+            string year=Console.ReadLine();
+            if(int.TryParse(year, out int number))
+            {
+                Console.WriteLine(" ASR: " + findasr(number));
+            }
+            else
+            {
+                Console.WriteLine(" Faqat butun son kirita olasiz !!!");
+            }
+            Main(args);
+
+        }
+        public static int findasr(int number)
+        {
+           return number / 100;
         }
     }
 }
